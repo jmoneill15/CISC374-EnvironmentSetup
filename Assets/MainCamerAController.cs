5ce470e0f645c19048fcd6f2865913b3c5e5dbfa
jmoneill15 <jmoneill28@gmail.com>
2025-02-10 00:58:24 -0500

    First commit and push

---
+++ Assets/MainCamerAController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCamerAController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Why did the programmer quit his job? \n Because he didn't get arrays.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
